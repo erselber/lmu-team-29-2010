143342e1f795025d86b3154c681e973358c0a6d8
jet-lee <mchedly@googlemail.com>
2010-05-29 00:31:29 +0200

    jet-lee 10

--- 4-2/debug/moc_myglwidget.cpp
+++ 4-2/debug/moc_myglwidget.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'myglwidget.h'
 **
-** Created: Thu 27. May 18:20:17 2010
+** Created: Thu 27. May 18:33:30 2010
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.6.2)
 **
 ** WARNING! All changes made in this file will be lost!
